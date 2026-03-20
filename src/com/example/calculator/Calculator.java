2552941a352ccff84ae2460f795073ce9767a958
usersy628 <kimsy628@gmail.com>
2026-03-20 10:57:51 +0900

    STEP 1 분기 수정

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package com.example.calculator;
 
+import java.util.InputMismatchException;
+import java.util.OptionalInt;
 import java.util.Scanner;
 
 public class Calculator {
@@ -15,46 +17,62 @@ public class Calculator {
         4. 반복문 사용으로 <"exit" 문자열 입력하기 전까지> 무한으로 계산하도록 수정하기
         */
 
-        boolean flag = true;
         int result = 0;
+        int num1;
+        int num2;
+        char symbol;
 
         Scanner sc = new Scanner(System.in);
 
-        while(flag == true){
+        while (true) {
+            try {
+                // 숫자인지
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                num1 = sc.nextInt();
 
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char symbol = sc.next().charAt(0);
-
-            // 연산 오류 방지: 기호 입력 받을 때 까지
-            while (!(symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' )) {
-                System.out.print("기호를 다시 입력하세요: ");
+                System.out.print("사칙연산 기호를 입력하세요: ");
                 symbol = sc.next().charAt(0);
-            }
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+                // 연산 오류 방지: 기호 입력 받을 때 까지
+                while (!(symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/')) {
+                    System.out.print("※기호를 다시 입력하세요: ");
+                    symbol = sc.next().charAt(0);
+                }
 
-            // 사칙연산 케이스 지정 ( addition / subtraction / multiply / division )
-            switch(symbol) {
-                case '+': result = num1 + num2;
-                    break;
-                case '-': result = num1 - num2;
-                    break;
-                case '*': result = num1 * num2;
-                    break;
-                case '/': result = num1 / num2;
-                    break;
-            }
-            System.out.println("결과: " + result); // 결과 반환
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                // 숫자인지 체크
+                num2 = sc.nextInt();
+                if (num2 == 0 && symbol == '/') {
+                    System.out.println("※0으로 나눌 수 없습니다");
+                }
 
-            System.out.println("더 계산하시겠습니까?");
-            System.out.println("[1] 계산하기");
-            System.out.println("[2] exit");
-            int resetNum = sc.nextInt();
-            if (resetNum == 2) {
-                break;
+                // 사칙연산 케이스 지정 ( addition / subtraction / multiply / division )
+                switch (symbol) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        result = num1 / num2;
+                        break;
+                }
+                System.out.println("결과: " + result); // 결과 반환
+
+                System.out.println("exit 입력 시 종료됩니다. 계속 하려면 아무거나 입력하세요.");
+                if (sc.next().equals("exit")) {
+                    System.out.println("프로그램 종료");
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("※양의 정수(0포함)를 입력해주세요.");
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println("※" + e.getMessage());
             }
         }
     }
